b809fc563d300a61d7c07af213046a31ffadc919
PrajjwalGupta <prajjwalg786@gmail.com>
2024-11-23 00:27:08 +0530

    TabBar added

---
+++ Twitter_Clone_node/View/Items/tabButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  tabButtonView.swift
+//  Twitter_Clone_node
+//
+//  Created by Prajjwal Gupta on 23/11/24.
+//
+
+import SwiftUI
+
+struct tabButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    tabButtonView()
+}
