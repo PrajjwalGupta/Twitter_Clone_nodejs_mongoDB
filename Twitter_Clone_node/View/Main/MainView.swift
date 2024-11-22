a746d1f4d158d6adeca878c96588e39ddffed63f
PrajjwalGupta <prajjwalg786@gmail.com>
2024-11-22 16:26:33 +0530

    Top Bar View Created

---
+++ Twitter_Clone_node/View/Main/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  Twitter_Clone_node
+//
+//  Created by Prajjwal Gupta on 22/11/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ Twitter_Clone_node/View/Main/TopBarView.swift
@@ -0,0 +1,18 @@
+//
+//  TopBarView.swift
+//  Twitter_Clone_node
+//
+//  Created by Prajjwal Gupta on 22/11/24.
+//
+
+import SwiftUI
+
+struct TopBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TopBarView()
+}
