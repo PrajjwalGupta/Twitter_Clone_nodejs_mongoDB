85e3456d2fd57850a77e1146248335805b4612f3
PrajjwalGupta <prajjwalg786@gmail.com>
2024-11-23 01:31:55 +0530

    Welcome Screen Completed

---
+++ Twitter_Clone_node/View/Authentication/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  Twitter_Clone_node
+//
+//  Created by Prajjwal Gupta on 23/11/24.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
