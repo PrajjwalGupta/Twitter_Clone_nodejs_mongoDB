f976d11a7a22f0705d109dd05c376c31f9bdab47
PrajjwalGupta <prajjwalg786@gmail.com>
2024-11-23 00:27:32 +0530

    TabButtonsAdded

--- Twitter_Clone_node/View/Items/tabButtonView.swift
+++ Twitter_Clone_node/View/Items/tabButtonView.swift
@@ -8,11 +8,26 @@
 import SwiftUI
 
 struct tabButtonView: View {
+    @Binding var currentTab: String
+    var animation: Namespace.ID
+    var title: String
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: {}, label: {
+            LazyVStack(spacing: 12){
+                Text(title).fontWeight(.semibold).foregroundColor(currentTab == title ? .blue : .gray)
+                    .padding(.horizontal)
+                if currentTab == title {
+                    Capsule()
+                        .fill(Color.blue).frame(height: 1.2).matchedGeometryEffect(id: "TAB", in: animation)
+                } else {
+                    Capsule()
+                        .fill(Color.clear).frame(height: 1.2)
+                }
+            }
+        })
     }
 }
 
-#Preview {
-    tabButtonView()
-}
+//#Preview {
+//    tabButtonView(currentTab: <#Binding<String>#>, title: "Tweets")
+//}
--- Twitter_Clone_node/View/Profile/UserProfile.swift
+++ Twitter_Clone_node/View/Profile/UserProfile.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct UserProfile: View {
     @State var offset: CGFloat = 0
     @State var titleOfset: CGFloat = 0
+    @State var currentTab = "Tweets"
+    @State var tabBarOffset: CGFloat = 0
+    @Namespace var animation
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
             VStack(spacing: 15) {
@@ -45,6 +48,79 @@ struct UserProfile: View {
                    // )
                 }.frame(height: 180)
                     .zIndex(1)
+                VStack{
+                    HStack {
+                        Image("me")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 75, height: 75)
+                            .clipShape(Circle())
+                            .padding(8)
+                            .background(Color.white.clipShape(Circle()))
+                            .offset(y: offset < 0 ? getoffset() - 20 : -20)
+                            .scaleEffect(getScale())
+                        Spacer()
+                        Button(action:{},label: {
+                            Text("Edit Profile")
+                        }).frame(width: 120, height: 40)
+                            .overlay( RoundedRectangle(cornerRadius: 40 / 2).stroke(Color("bg"), lineWidth: 2)).padding()
+                        
+                        
+                    }.padding(.top, -25)
+                        .padding(.bottom, -10)
+                    VStack(alignment: .leading) {
+                        Text("Prajjwal")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundStyle(.primary)
+                        Text("@Prajjwal1")
+                            .foregroundStyle(.gray)
+                        Text("Lorem Ipsum is simply dummy text of the printing and 4️⃣, 5️⃣typesetting industry.")
+                        HStack(spacing: 5) {
+                            Text("54")
+                                .foregroundStyle(.primary)
+                                .fontWeight(.semibold)
+                            Text("Followers")
+                                .foregroundStyle(.gray)
+                            Text("234")
+                                .foregroundStyle(.primary)
+                                .fontWeight(.semibold)
+                                .padding(.leading, 10)
+                            Text("Following")
+                                .foregroundStyle(.gray)
+                        }.padding()
+                    }.overlay (
+                        GeometryReader { proxy -> Color in
+                            let minY = proxy.frame(in: .global).minY
+                            DispatchQueue.main.async {
+                                self.titleOfset = minY
+                            }
+                            return Color.clear
+                        }.frame(width: 0,height: 0), alignment: .top
+                    )
+                    VStack(spacing: 0) {
+                        ScrollView(.horizontal, showsIndicators: true) {
+                            HStack(spacing: 0) {
+                                tabButtonView(currentTab: $currentTab, animation: animation, title: "Tweets")
+                                tabButtonView(currentTab: $currentTab, animation: animation, title: "Tweets & Likes")
+                                tabButtonView(currentTab: $currentTab, animation: animation, title: "Media")
+                                tabButtonView(currentTab: $currentTab, animation: animation, title: "Likes")
+                            }
+                        }
+                        Divider()
+                    }.padding(.top, 30)
+                        .background(Color.white)
+                        .offset(y: tabBarOffset < 90 ? -tabBarOffset + 90 : 0)
+                        .overlay (
+                            GeometryReader { proxy -> Color in
+                                let minY = proxy.frame(in: .global).minY
+                                DispatchQueue.main.async {
+                                    self.tabBarOffset = minY
+                                }
+                                return Color.clear
+                            }.frame(width: 0, height: 0), alignment: .top
+                        ).zIndex(1)
+                }
             }
         }
     }
@@ -56,8 +132,18 @@ struct UserProfile: View {
         let progress = 20 / titleOfset
         let offset = 60 * (progress > 0 && progress <= 1 ? progress : 1)
         return offset
+    }
+    func getoffset() -> CGFloat {
+        let progress = (-offset / 80) * 20
+        return progress <= 20 ? progress : 20
+    }
+    func getScale() -> CGFloat {
+        let progress = -offset / 80
+        let scale = 1.8 - (progress < 1.0 ? progress : 1.0)
+        return scale < 1 ? scale : 1
         
     }
+    
 }
 
 #Preview {
