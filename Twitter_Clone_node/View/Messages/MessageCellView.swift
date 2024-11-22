edf73eb555e8d56980ccb8c4c01d527d3b40996d
PrajjwalGupta <prajjwalg786@gmail.com>
2024-11-22 15:18:41 +0530

    Message View Complete

---
+++ Twitter_Clone_node/View/Messages/MessageCellView.swift
@@ -0,0 +1,18 @@
+//
+//  MessageCellView.swift
+//  Twitter_Clone_node
+//
+//  Created by Prajjwal Gupta on 22/11/24.
+//
+
+import SwiftUI
+
+struct MessageCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageCellView()
+}
