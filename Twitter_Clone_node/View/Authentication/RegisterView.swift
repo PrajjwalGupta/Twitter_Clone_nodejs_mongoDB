e9258ae48724375f210010cb35e7d2984e9b8c9d
PrajjwalGupta <prajjwalg786@gmail.com>
2024-11-23 02:05:42 +0530

    create account view created

---
+++ Twitter_Clone_node/View/Authentication/RegisterView.swift
@@ -0,0 +1,62 @@
+//
+//  RegisterView.swift
+//  Twitter_Clone_node
+//
+//  Created by Prajjwal Gupta on 23/11/24.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    @State var name = ""
+    @State var email = ""
+    @State var password = ""
+    var body: some View {
+        VStack {
+            ZStack {
+                HStack {
+                    Button(action: {}, label: {
+                        Text("Cancel")
+                            .foregroundColor(.blue)
+                    })
+                    Spacer()
+                }.padding(.horizontal)
+                Image("logo")
+                    .resizable()
+                    .scaledToFill()
+                    .padding(.trailing)
+                    .frame(width: 40, height: 40)
+            }
+            Text("Create your account")
+                .font(.title)
+                .bold()
+                .padding(.top, 35)
+            VStack (alignment: .leading) {
+                CustomAuthTextField(placeholder: "Name", text: $name)
+                CustomAuthTextField(placeholder: "Phone number or email", text: $email)
+                SecureAuthTextField(placeholder: "Password", text: $password)
+            }
+            Spacer(minLength: 0)
+            VStack {
+                Rectangle()
+                    .frame(height: 1)
+                    .foregroundColor(.gray)
+                HStack{
+                    Spacer()
+                    Button(action: {}, label: {
+                        Capsule().frame(width: 100, height: 40, alignment: .center)
+                            .foregroundColor(Color(red: 29/255, green: 161/255, blue: 242/255))
+                            .overlay {
+                                Text("Next")
+                                    .foregroundStyle(.white)
+                            }
+                    }).padding(.trailing, 24)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    RegisterView()
+}
---
+++ Twitter_Clone_node/View/Items/CustomAuthTextField.swift
@@ -0,0 +1,34 @@
+//
+//  CustomAuthTextField.swift
+//  Twitter_Clone_node
+//
+//  Created by Prajjwal Gupta on 23/11/24.
+//
+
+import SwiftUI
+
+struct CustomAuthTextField: View {
+    var placeholder: String
+    @Binding var text: String
+    var body: some View {
+        VStack {
+            ZStack(alignment: .leading, content: {
+                Text(placeholder)
+                if text.isEmpty {
+                    Text(placeholder).foregroundColor(.gray)
+                }
+                TextField("", text: $text)
+                    .frame(height: 45)
+                    .foregroundColor(Color(red: 29/255, green: 161/255, blue: 242/255))
+            })
+            Rectangle()
+                .frame(height: 1, alignment: .center)
+                .foregroundColor(.gray)
+                .padding(.top, -2)
+        }.padding(.horizontal)
+    }
+}
+
+//#Preview {
+//    CustomAuthTextField()
+//}
